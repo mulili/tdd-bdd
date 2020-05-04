1315f3594576f83ccbaad923f511ad629f3187ce
mulili <muli9611@gmail.com>
2020-05-04 19:47:15 +0800

    add undoList compmoennt style

--- jest-react/src/component/UndoList/index.js
+++ jest-react/src/component/UndoList/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './index.css';
 class UndoList extends Component {
   handleDelete = index => {
     const { deleteItem } = this.props;
@@ -7,26 +8,35 @@ class UndoList extends Component {
   render() {
     const { undoList } = this.props;
     return (
-      <div>
-        <span>正在进行</span>
-        <span className="counter" data-test="counter">
-          {undoList.length}
-        </span>
-        <ul>
-          {undoList.map((item, index) => (
-            <div key={`${index}${item}`}>
-              <li data-test="undoList">{item}</li>
-              <button
-                data-test="deleteItem"
-                onClick={() => {
-                  this.handleDelete(index);
-                }}
+      <div className="undo-list">
+        <div className="undo-list-container">
+          <div className="undo-list-title">
+            正在进行
+            <span className="undo-list-counter" data-test="counter">
+              {undoList.length}
+            </span>
+          </div>
+          <ul className="undo-list-content">
+            {undoList.map((item, index) => (
+              <li
+                key={`${index}${item}`}
+                className="undo-list-item"
+                data-test="undoList"
               >
-                X
-              </button>
-            </div>
-          ))}
-        </ul>
+                {item}
+                <div
+                  className="delete-item"
+                  data-test="deleteItem"
+                  onClick={() => {
+                    this.handleDelete(index);
+                  }}
+                >
+                  -
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
